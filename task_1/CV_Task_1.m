d82317c2dab7ac2a392a77c9ccb184a5f206e46e
Owen Prosser <6135815+owenprosser@users.noreply.github.com>
2019-04-25 17:06:41 +0100

    Completed Bottom-hat filtering and K-means segmentation

--- task_1/CV_Task_1.m
+++ task_1/CV_Task_1.m
@@ -60,39 +60,48 @@ for i = 1:size(imagesArray, 2)
      title("Ground Truth");
     count = count + 1;
 end
-
-i = 3;
-currentImage = imagesArray{i};
-currentImage = rgb2gray(currentImage);
-se = strel('disk',3);
-currentImage = imsubtract(imadd(currentImage,imbothat(currentImage,se)),imtophat(currentImage,se));
-[L,Centers] = imsegkmeans(currentImage,3);
-currentImage = labeloverlay(currentImage,L);
 figure;
-imshow(currentImage);
-figure;
-
-lab_he = rgb2lab(imagesArray{i});
-ab = lab_he(:,:,2:3);
-ab = im2single(ab);
-nColors = 3;
-% repeat the clustering 3 times to avoid local minima
-pixel_labels = imsegkmeans(ab,nColors,'NumAttempts',3);
-imshow(pixel_labels,[])
-title('Image Labeled by Cluster Index');
-mask1 = pixel_labels==1;
-cluster1 = imagesArray{i} .* uint8(mask1);
-imshow(cluster1)
-title('Objects in Cluster 1');
-
-mask2 = pixel_labels==2;
-cluster2 = imagesArray{i} .* uint8(mask2);
-figure;
-imshow(cluster2)
-title('Objects in Cluster 2');
-
-mask3 = pixel_labels==3;
-cluster3 = imagesArray{i} .* uint8(mask3);
-figure;
-imshow(cluster3)
-title('Objects in Cluster 3');
+count = 1;
+for i = 1:size(imagesArray, 2)
+    currentImage = imagesArray{i};
+    se = strel('disk',3);
+    
+    lab_he = rgb2lab(currentImage);
+    lab_he = imsubtract(imadd(lab_he,imbothat(lab_he,se)),imtophat(lab_he,se));
+    ab = lab_he(:,:,2:3);
+    ab = im2single(ab);
+    nColors = 3;
+    % repeat the clustering 3 times to avoid local minima
+    pixel_labels = imsegkmeans(ab,nColors,'NumAttempts',3);
+    mask2 = pixel_labels==2;
+    currentImage = currentImage .* uint8(mask2);
+    
+    cform = makecform('srgb2lab');
+    lab = applycform(currentImage, cform);
+    currentImage = lab;
+    currentImage = rgb2gray(currentImage);
+    currentImage = imbinarize(currentImage);
+    currentImage = imfill(currentImage, 'holes');
+    currentImage = bwareafilt(currentImage, 1);
+    
+    currentImageDouble = im2double(currentImage);
+    currentGround = im2double(groundTruthArray{i});
+    diceScore = dice(currentImageDouble, currentGround);
+    disp(diceScore);
+    
+    subplot(3, 3, count);
+    sgtitle('Bottom-hat filtering and K-means segmentation')
+    imshow(currentImage);
+    title("Segmented: " + diceScore);
+    count = count + 1;
+    
+    subplot(3, 3, count);
+    imshow(imagesArray{i});
+    title("Original");
+    count = count + 1;
+    
+    subplot(3, 3, count);
+    imshow(groundTruthArray{i});
+     title("Ground Truth");
+    count = count + 1;
+end
